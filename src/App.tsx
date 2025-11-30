204c78fc1b98ec8f3f4d6798574caceff0efc180
JulesSantana <iuliututor@gmail.com>
2025-11-30 09:41:34 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
+import { Services } from './components/Services';
 import { IndibaPage } from './components/IndibaPage';
 import { About } from './components/About';
 import { Prices } from './components/Prices';
@@ -35,6 +36,7 @@ function App() {
           <Header />
           <main>
           <Hero onOpenPriceGuide={() => setIsPriceGuideOpen(true)} />
+          <Services />
           <IndibaPage />
           <About />
           <Prices />
