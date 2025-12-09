39f0df92eee062b593a068ff9a2d5b096dc6d54c
JulesSantana <iuliututor@gmail.com>
2025-12-09 19:43:52 +0100

    Updated LogoBanner.tsx

--- src/components/LogoBanner.tsx
+++ src/components/LogoBanner.tsx
@@ -5,7 +5,7 @@ export function LogoBanner() {
         <div
           className="relative w-full rounded-2xl overflow-hidden border border-[#D4AF37]/30 hover:border-[#D4AF37]/60 transition-all duration-500 hover:shadow-[0_0_40px_rgba(212,175,55,0.3)] aspect-[2/1]"
           style={{
-            backgroundImage: 'url(/chatgpt_image_nov_19,_2025,_10_16_50_am.png)',
+            backgroundImage: 'url(/chatgpt_image_nov_19,_2025,_10_16_50_am%20copy%20copy.png)',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat'
